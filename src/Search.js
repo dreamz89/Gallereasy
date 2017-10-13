ad146afb39305381f8e8886b191b33a2b451b342
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-10-13 16:03:32 +0800

    search field results API results

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react'
+import fetch from 'isomorphic-fetch'
 
 export default class Search extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: '',
+      pictures: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.keyPress = this.keyPress.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({ value: e.target.value })
+  }
+
+  keyPress (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault()
+      this.searchResult()
+    }
+  }
+
+  searchResult (e) {
+    var input = this.state.value
+    const url = 'http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=FHvvDNm13xAj2exfW1h4gykFJWXMz3W3&limit=8'
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      let pictures = result.data.map((pic) => {
+        return (
+          <div key={pic.id}>
+            <img src={pic.images.original_still.url} alt={this.state.value} />
+          </div>
+        )
+      })
+      this.setState({pictures: pictures})
+    })
+  }
+
   render () {
     return (
       <div>
-        <form action='GET'>
-          <input placeholder='Start searching for images!' type="text" />
-       </form>
+        <form>
+          <input placeholder='Start searching for images!' type='text' value={this.state.value} onKeyDown={this.keyPress} onChange={this.handleChange} />
+        </form>
+        {this.state.pictures}
       </div>
     )
   }
